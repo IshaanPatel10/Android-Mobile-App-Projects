eef47191962cb8b6612ee35ad12b7664ba4f351b
IshaanPatel10 <ishaanpatel7007@gmail.com>
2023-10-08 17:55:37 -0500

    MulchApp
    Mulch

---
+++ Apps/app/src/main/java/edu/noctrl/fall23_330/mulch_patel_ishaan/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.noctrl.fall23_330.mulch_patel_ishaan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
