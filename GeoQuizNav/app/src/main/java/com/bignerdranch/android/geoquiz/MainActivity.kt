55555086558ac80ecb50de4448747ba3c7bc746e
IshaanPatel10 <ishaanpatel7007@gmail.com>
2023-11-09 18:23:45 -0600

    nav
    nav

---
+++ GeoQuizNav/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.bignerdranch.android.geoquiz
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(this.layoutInflater)
+        setContentView(binding.root)
+    }
+
+    // These functions are overridden for demo purposes only
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+}
---
+++ GeoQuizNav/app/src/main/java/com/bignerdranch/android/geoquiz/QuestionFragment.kt
@@ -0,0 +1,126 @@
+package com.bignerdranch.android.geoquiz
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
+import androidx.navigation.fragment.findNavController
+import com.bignerdranch.android.geoquiz.databinding.FragmentQuestionBinding
+
+private const val TAG = "QuestionFragment"
+private const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
+
+/**
+ * The QuestionFragment is the start destination.
+ */
+class QuestionFragment : Fragment() {
+    // TODO Use view binding in fragments:
+    //  https://developer.android.com/topic/libraries/view-binding#fragments
+    private var _binding: FragmentQuestionBinding? = null
+
+    // TODO This property is only valid between onCreateView and
+    //  onDestroyView.
+    private val binding get() = _binding!!
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true),
+    )
+    private var currentIndex = 0
+    private var isCheater = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO Pass results between fragments
+        //  https://developer.android.com/guide/fragments/communicate#fragment-result
+        //  The listener will receive the result from the CheatFragment
+        //  The request key must be the same in both fragments
+        setFragmentResultListener(CheatFragment.CHEAT_RESULT_KEY) { _, bundle ->
+            // We use a Boolean here, but any type that can be put in a Bundle is supported.
+            isCheater = bundle.getBoolean(CheatFragment.IS_CHEATER)
+            Log.d(TAG, "isCheater: $isCheater")
+        }
+    }
+
+    // TODO It is recommended to only inflate the layout in this method
+    //  and move logic that operates on the returned View to onViewCreated(View, Bundle).
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentQuestionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    // TODO This gives subclasses a chance to initialize themselves once
+    //  they know their view hierarchy has been completely created.
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // TODO Saved instance state should be restored here after view is created.
+        if (savedInstanceState != null) {
+            Log.d(TAG, "savedInstanceState is set.")
+            currentIndex = savedInstanceState.getInt(CURRENT_INDEX_KEY, 0)
+        }
+        binding.questionText.setText(questionBank[currentIndex].testResId)
+
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            binding.questionText.setText(questionBank[currentIndex].testResId)
+        }
+
+        binding.trueButton.setOnClickListener {
+            checkAnswer(true)
+        }
+
+        binding.falseButton.setOnClickListener {
+            checkAnswer(false)
+        }
+
+        binding.cheatButton.setOnClickListener {
+            // TODO Navigate to the CheatFragment passing the answer as an argument
+            val answer = questionBank[currentIndex].answer
+            val action =
+                QuestionFragmentDirections.actionQuestionFragmentToCheatFragment(answer)
+            this.findNavController().navigate(action)
+        }
+
+    }
+
+    // TODO Called when the view previously created by onCreateView
+    //  has been detached from the fragment.
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // The view binding is no longer valid because the view is detached from the fragment
+        _binding = null
+    }
+
+    // Save data between configuration changes.
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState")
+        outState.putInt(CURRENT_INDEX_KEY, this.currentIndex)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val resId = when {
+            isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
+        }
+        isCheater = false
+
+        Toast.makeText(this.context, resId, Toast.LENGTH_SHORT).show()
+    }
+
+}
